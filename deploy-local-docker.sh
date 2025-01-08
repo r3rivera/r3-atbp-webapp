4cc1c35a9a977173dd2b3bc9b3f9b1e4674e79f2
R2 Rivera <r.ryan.rivera@gmail.com>
2025-01-07 21:00:29 -0600

    Added docker file

---
+++ deploy-local-docker.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+echo ""
+echo ""
+JAR_FILE="$(find ./target/*.jar | head -n 1 | cut -d '/' -f 3)"
+echo ""
+echo ""
+echo "Current Docker Version"
+docker --version
+echo ""
+echo "Current Directory is $(pwd)"
+echo "Current User is $USER"
+echo ""
+echo "Start Creating Docker Image"
+chmod +x ./Dockerfile
+DOCKER_TAG="r3app-atbp"
+
+echo "Stopping an existing docker container..."
+DOCKER_CTR_STOP="$(docker container stop ${DOCKER_TAG} | cut -d ':' -f 2)"
+echo "Stop Status :: ${DOCKER_CTR_STOP}"
+
+echo "Removing stopped container..."
+DOCKER_CTR_RM="$(docker container rm ${DOCKER_TAG})"
+echo "Remove Status :: ${DOCKER_CTR_RM}"
+
+DOCKER_CURR="$(docker image rm ${DOCKER_TAG})"
+echo "Removing existing docker images..."
+echo "Remove Status :: ${DOCKER_CURR}"
+
+GIT_COMMIT_LABEL="$(git log | head -n 1 | cut -d '/' -f 2)"
+
+docker build -t ${DOCKER_TAG} --build-arg jar_name=${JAR_FILE} --no-cache -f ./Dockerfile .
+echo "Done Creating Docker Image"
+echo ""
+echo ""
+echo "Deploying a new docker container...."
+DOCKER_UUID="$(docker container run -p 8081:8081 -d --name ${DOCKER_TAG} ${DOCKER_TAG})"
+echo "New Docker ID deployed :: ${DOCKER_UUID}"
