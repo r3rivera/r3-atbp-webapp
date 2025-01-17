8b324d5dd38d61144b51f4006d3602f9912d4cc1
R2 Rivera <r.ryan.rivera@gmail.com>
2025-01-16 21:32:27 -0600

    Added config to disable cors

---
+++ src/main/java/com/r3projects/atbp/configs/WebAppConfiguration.java
@@ -0,0 +1,26 @@
+package com.r3projects.atbp.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebAppConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+
+            }
+        };
+    }
+}
